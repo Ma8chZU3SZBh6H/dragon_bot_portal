b085caf022838ad28e3ded4681bad579a0cf9736
skindervik <38836685+skindervik@users.noreply.github.com>
2021-09-21 00:19:03 +0300

    connected socket io

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { io } from "socket.io-client";
+const socket = io("localhost:7000");
 
 ReactDOM.render(
   <React.StrictMode>
